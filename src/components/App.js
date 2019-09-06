23d5cc96a509af751ee31ad08ddd888535040f76
Tae Lee <taebumlee@gmail.com>
2019-09-06 12:59:33 -0700

    Now, back to router for App

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Home from "./Home";
 import ListKeg from "./ListKeg/ListKeg";
+import {Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Home/>
-        <ListKeg/>
+        <Route exact path='/' component={ListKeg}/>
       </header>
     </div>
   );
