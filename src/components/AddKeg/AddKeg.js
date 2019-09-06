ab77a642b97ae4ee6d9e0d7b7ae1bfe5403eb9ff
Tae Lee <taebumlee@gmail.com>
2019-09-06 13:50:22 -0700

    create add keg form with css

---
+++ src/components/AddKeg/AddKeg.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./AddKeg.css";
+
+function AddKeg() {
+  return (
+    <div className='AddKeg'>
+      <form>
+        <input type='text' id='name' placeholder='Name'/>
+        <input type='text' id='brand' placeholder='Brand'/>
+        <input type='number' id='alcoholContent' placeholder='%'/>
+        <input type='number' id='price' placeholder='Price'/>
+        <input type='number' id='pints' placeholder='Pints'/>
+        <button type='submit'>Add Keg</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddKeg;
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import Home from "./Home";
 import Header from "./Header";
 import ListKeg from "./ListKeg/ListKeg";
 import {Route, Switch} from "react-router-dom";
-import Error404 from '../Error404';
+import Error404 from "../Error404";
+import AddKeg from "./AddKeg/AddKeg"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <Switch>
           <Route exact path='/' component={ListKeg}/>
+          <Route path='/addkeg' component={AddKeg}/>
           <Route component={Error404} />
         </Switch>
       </header>
